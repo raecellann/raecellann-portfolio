009533a6c0e98d0fb8e3de6b5dd9c71f666e6767
raecellann <raecellanndomingogalvez@gmail.com>
2026-04-10 01:27:24 +0800

    Fix resume deployment - rename to resume.pdf and update .gitignore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,7 @@ import FakeNewsDetectorImage from "../assets/works/Fake news-detector.png";
 import FakeNewsDetector1Image from "../assets/works/Fake news detector1.png";
 import FakeNewsDetector2Image from "../assets/works/Fake news-detector2.png";
 import studyingGif from "../assets/studying.gif";
-import resumeFile from "../assets/RAECELL ANN GALVEZ - RESUME.pdf";
+import resumeFile from "../assets/resume.pdf";
 
 const projects = [
   {
